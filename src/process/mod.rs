3dfd4af8f1a5a3ab46af181a91a6d1aecbd6be76
Saurabhatgit <saurabhsingh1641998@gmail.com>
2022-06-29 17:43:51 +0530

    initial commit

---
+++ src/process/mod.rs
@@ -0,0 +1,10 @@
+use configuration_parameters::ConfigurationParameters;
+use slog::Logger;
+
+pub fn parameter_calls(
+    config_params: &ConfigurationParameters,
+    logger: &Logger,
+    diag_logger: &Logger,
+) {
+    println!("hello world");
+}
